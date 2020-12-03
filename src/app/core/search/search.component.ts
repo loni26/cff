813dde1eee6a029c59f9eba35f477ce2d8e9c8ce
Nilo Castillo <nilo.castill026@gmail.com>
2020-12-03 09:41:09 +0100

    autocomplete

--- src/app/core/search/search.component.ts
+++ src/app/core/search/search.component.ts
@@ -1,8 +1,10 @@
+import { SuggestionSearchService } from './../services/suggestionSearch/suggestion-search.service';
 import { ResearchService } from './../services/research/research.service';
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
 import { Router } from '@angular/router';
 import { first } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-search',
@@ -13,8 +15,13 @@ export class SearchComponent implements OnInit {
   title: string = 'CFF Mobile PWA';
 
   form: FormGroup;
+  cityFrom: string;
+  cityTo: string;
+  
+
   constructor(
     private _api: ResearchService,
+    private _api2: SuggestionSearchService,
     private _router: Router
   ) { }
 
@@ -33,6 +40,39 @@ export class SearchComponent implements OnInit {
     console.log('result: ', result);
     if(!result?.connections) return;
     this._router.navigate(['result']);
+    }
+
+    async getCity (divId: string){
+      const startLen = 2;
+      if(divId ==='from'){
+        const term = this.form.value.from;
+        
+        if(term.length>=startLen){
+          console.log(term);
+          this.cityFrom = await this._api2.autoComplete(term);
+        }else {
+          this.cityFrom = null; 
+        }
+      }else {
+        const term = this.form.value.to;
+        if(term.length >= startLen){
+          this.cityTo = await this._api2.autoComplete(term);
+        } else {
+          this.cityTo = null;
+        }
+      }
+    } 
+
+    setCity(name: string, divId: string){
+      if (divId === 'from') {
+        this.form.patchValue({['from']:name})
+        this.cityFrom = null;
+      }
+
+      if (divId === 'to') {
+        this.form.patchValue({['to']: name})
+        this.cityTo = null;
+      }
     }
 
 }
--- src/app/core/services/suggestionSearch/suggestion-search.service.ts
+++ src/app/core/services/suggestionSearch/suggestion-search.service.ts
@@ -1,9 +1,19 @@
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SuggestionSearchService {
+ 
 
-  constructor() { }
+  constructor(private _http: HttpClient) { }
+
+  async autoComplete(value: string){
+    
+    const url = `https://timetable.search.ch/api/completion.fr.json?term=${value}`;
+    const result = await this._http.get(url).toPromise().catch(err => err);
+    return result;
+  }
 }
